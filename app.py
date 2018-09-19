5b0ee7957d27dc8017b989ea93702adb54bff99e
Arno Alexander <arno2alexander@gmail.com>
2018-09-19 12:39:00 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, send_file, request
+
+app = Flask(__name__)
+
+@app.route('/blackwhite', methods=['POST'])
+def blackwhite():
+	if 'media' in request.files:
+		return send_file(
+			'bebek.jpg',
+			mimetype='image/jpeg'
+		)
